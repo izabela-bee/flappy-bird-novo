d112e6ce85a7cf46303ac3a6be8bfc726c6fe04f
iza <izabelamartins.ifmg@gmail.com>
2025-01-15 11:18:39 -0300

    1 versao

---
+++ jogo.js
@@ -0,0 +1,20 @@
+const sprites = new Image();
+sprites.src = './sprites.png';
+
+const canvas = document.querySelector('#game-canvas');
+const contexto = canvas.getContext('2d');
+
+function loop(){
+
+    contexto.drawImage(
+        sprites,
+        0, 0,
+        35, 25,
+        10, 50, 
+        35, 25);
+
+        requestAnimationFrame(loop);
+    
+}
+
+loop();
