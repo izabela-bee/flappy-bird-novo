0e942f27b02fe7b8137334483d8ed09f9126aafd
iza <izabelamartins.ifmg@gmail.com>
2025-01-15 12:16:40 -0300

    3versao

--- jogo.js
+++ jogo.js
@@ -22,11 +22,70 @@ const flappyBird = {
             );
         }
     }
-    function loop(){
-        
-        flappyBird.desenha();
-        
-        requestAnimationFrame(loop);
+
+    const chao = {
+        spriteX: 0,
+        spriteY: 609,
+        largura: 223,
+        altura: 115,
+        x: 0,
+        y: 380,
+        desenhachao(){
+            contexto.drawImage(
+                sprites,
+                chao.spriteX, chao.spriteY,
+                chao.largura, chao.altura,
+                chao.x, chao.y,
+                chao.largura, chao.altura,
+                );
+
+                contexto.drawImage(
+                    sprites,
+                    chao.spriteX, chao.spriteY,
+                    chao.largura, chao.altura,
+                    chao.x+chao.largura, chao.y,
+                    chao.largura, chao.altura,
+                    );
+
+        }
     }
+
+        const planoFundo = {
+            spriteX: 400,
+            spriteY: 0,
+            largura: 265,
+            altura: 204,
+            x: 0,
+            y: 177,
+            desenhafundo(){
+                contexto.drawImage(
+                    sprites,
+                    planoFundo.spriteX, planoFundo.spriteY,
+                    planoFundo.largura, planoFundo.altura,
+                    planoFundo.x, planoFundo.y,
+                    planoFundo.largura, planoFundo.altura,
+                    );
+
+                    contexto.drawImage(
+                        sprites,
+                        planoFundo.spriteX, planoFundo.spriteY,
+                        planoFundo.largura, planoFundo.altura,
+                        planoFundo.x+planoFundo.largura, planoFundo.y,
+                        planoFundo.largura, planoFundo.altura,
+                        );
+
+                }
+            }
+
+
+
+function loop(){
+        
+    flappyBird.desenha();
+    chao.desenhachao();
+    planoFundo.desenhafundo();
+        
+    requestAnimationFrame(loop);
+}
         
 loop();
