ffa345d5e505c28b112defa9db57727c3f31cf97
iza <izabelamartins.ifmg@gmail.com>
2025-01-15 11:36:23 -0300

    2 versao

--- jogo.js
+++ jogo.js
@@ -4,17 +4,29 @@ sprites.src = './sprites.png';
 const canvas = document.querySelector('#game-canvas');
 const contexto = canvas.getContext('2d');
 
-function loop(){
 
-    contexto.drawImage(
-        sprites,
-        0, 0,
-        35, 25,
-        10, 50, 
-        35, 25);
-
+const flappyBird = {
+    spriteX: 0,
+    spriteY: 0,
+    largura: 35,
+    altura: 25,
+    x: 10,
+    y: 50,
+    desenha(){
+        contexto.drawImage(
+            sprites,
+            flappyBird.spriteX, flappyBird.spriteY,
+            flappyBird.largura, flappyBird.altura,
+            flappyBird.x, flappyBird.y,
+            flappyBird.largura, flappyBird.altura,
+            );
+        }
+    }
+    function loop(){
+        
+        flappyBird.desenha();
+        
         requestAnimationFrame(loop);
-    
-}
-
+    }
+        
 loop();
